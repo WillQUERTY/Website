7da9d5b71e212995f1aa80924af683e50333d25d
William Andres Peñaranda Oñate <166168844+WillQUERTY@users.noreply.github.com>
2024-05-16 03:46:37 +0000

    actualizar sistema

--- Javascript/login.js
+++ Javascript/login.js
@@ -7,10 +7,12 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     fetch('credentials.txt')
         .then(response => response.text())
         .then(data => {
+            console.log(data); // Ver qué se está obteniendo del archivo credentials.txt
             var credentials = data.split('\n');
-            var storedUsername = credentials[0];
-            var storedPassword = credentials[1];
+            var storedUsername = credentials[0].trim(); // Usar trim() para eliminar espacios en blanco
+            var storedPassword = credentials[1].trim(); // Usar trim() para eliminar espacios en blanco
 
+            console.log(username, password, storedUsername, storedPassword); // Verificar los valores antes de la comparación
             var statusElement = document.getElementById('status');
             if (username === storedUsername && password === storedPassword) {
                 statusElement.style.backgroundColor = 'green';
